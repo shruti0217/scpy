70f91fe6a751c67b7e9955f145f7d18074dbf510
Shruti <shrutigehlot2629@gmail.com>
2021-02-18 03:00:36 +0530

    updated

--- duplicate_ls.py
+++ duplicate_ls.py
@@ -11,7 +11,7 @@ def find_dup(arr):
     assert type(arr) is not str 
     assert  len(arr) > 0 and len(arr)<=100,f"Shouldn't be empty or greater than 100"
     
-    
+    t=0
     arr_d=[]
    
     for i in range(0,len(arr)):
@@ -20,8 +20,38 @@ def find_dup(arr):
             arr_d.append(arr[i])'''
         if arr.count(arr[i])>1 and not arr[i] in arr_d:
             arr_d.append(arr[i])
+        t+=1
+    print(f"takes {t} rounds of loop")
             
     return arr_d
+
+def find_dup_2(arr):    #[5,2,4,5,1,2]
+    arr.sort()          #[1,2,2,4,5,5]
+    arr_d=[]
+    test=0
+    #t=0
+    
+    '''for i in range(0,len(arr)):
+        if arr.count(arr[i]) > 1 :
+            arr_d.append(arr[i])
+            i=i+arr.count(arr[i])-1     #this  creates another variable instead of updating the one that already exist 
+        test+=1
+    print(test)'''
+    while test < len(arr) :                 
+        if arr.count(arr[test]) > 1 :       
+            arr_d.append(arr[test])
+            test = test + (arr.count(arr[test]))
+        else :   
+            test += 1
+       
+        #t+=1
+      
+    
+    #print(f"takes {t} rounds of loop") # takes t=len(arr) times to complete it 
+    return arr_d
+        
+    
+
 if __name__=="__main__":
     array=[]
     n=0
@@ -40,6 +70,5 @@ if __name__=="__main__":
 
     print(f"You entered : {array}:")
     print(find_dup(array)) 
-    
-
+    print(f"Now from the second function: {find_dup_2(array)}")    
 
